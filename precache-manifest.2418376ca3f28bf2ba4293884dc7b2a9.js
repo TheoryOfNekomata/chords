36cc888699b64c8435d26bdf3bd3e59ae141468b
TheoryOfNekomata <allan.crisostomo@outlook.com>
2019-11-04 19:54:12 +0800

    Updates

---
+++ precache-manifest.2418376ca3f28bf2ba4293884dc7b2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96251ed37b2f895b92a83ec770af42a9",
+    "url": "/chords/index.html"
+  },
+  {
+    "revision": "a12b8cb5176bf2cc4806",
+    "url": "/chords/static/css/2.55183701.chunk.css"
+  },
+  {
+    "revision": "561c8ce44a253e9e16bd",
+    "url": "/chords/static/css/main.9afff407.chunk.css"
+  },
+  {
+    "revision": "a12b8cb5176bf2cc4806",
+    "url": "/chords/static/js/2.2194f9cb.chunk.js"
+  },
+  {
+    "revision": "561c8ce44a253e9e16bd",
+    "url": "/chords/static/js/main.3f537c2a.chunk.js"
+  },
+  {
+    "revision": "0d8a1a7c2ddde8b3a8f6",
+    "url": "/chords/static/js/runtime-main.0b35ac78.js"
+  }
+]);
